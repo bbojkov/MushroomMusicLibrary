bd55a5fe708c11ee374294d02f438b5cf6201701
pollx <dimova.paola@gmail.com>
2017-02-22 03:54:29 +0200

    SearchDisplayPresenter tests added

---
+++ MusicLibrary.Tests/MVP/SearchDisplayPresenterTests/Constructor_Should.cs
@@ -0,0 +1,55 @@
+﻿namespace MusicLibrary.Tests.MVP.SearchDisplayPresenterTests
+{
+    using Moq;
+    using MusicLibrary.MVP.Presenters;
+    using MusicLibrary.MVP.Views;
+    using MusicLibrary.Services;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using WebFormsMvp;
+
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Constructor_ShouldThowsArgumentNullExceptionWhenBandServiceIsNull()
+        {
+            //Arange
+            IBandService bandService = null;
+            var viewMock = new Mock<ISearchDisplayView>();
+
+            //Act & Assert
+            Assert.That(() => new SearchDisplayPresenter(viewMock.Object, bandService), Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+
+        [Test]
+        public void Constructor_ShouldNotThrowWhenServicesAreValid()
+        {
+            //Arange
+            var bandServiceMock = new Mock<IBandService>();
+            var viewMock = new Mock<ISearchDisplayView>();
+
+            //Act & Assert
+            Assert.That(() => new SearchDisplayPresenter(viewMock.Object, bandServiceMock.Object), Throws.Nothing);
+        }
+
+        [Test]
+        public void Constructor_ShouldCreatePresenterWhenServicesAreValid()
+        {
+            //Arange
+            var bandServiceMock = new Mock<IBandService>();
+            var viewMock = new Mock<ISearchDisplayView>();
+
+            //Act
+            var presenter = new SearchDisplayPresenter(viewMock.Object, bandServiceMock.Object);
+
+            // Assert
+            Assert.That(presenter, Is.InstanceOf<Presenter<ISearchDisplayView>>());
+        }
+    }
+}
---
+++ MusicLibrary.Tests/MVP/SearchDisplayPresenterTests/View_OnSearchDisplayBands_Should.cs
@@ -0,0 +1,103 @@
+﻿namespace MusicLibrary.Tests.MVP.SearchDisplayPresenterTests
+{
+    using Moq;
+    using MusicLibrary.Models;
+    using MusicLibrary.MVP.EventArguments;
+    using MusicLibrary.MVP.Models;
+    using MusicLibrary.MVP.Presenters;
+    using MusicLibrary.MVP.Views;
+    using MusicLibrary.Services;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class View_OnSearchDisplayBands_Should
+    {
+        [Test]
+        public void OnSearchDisplayBands_ShouldCallSearchBandsByBandName()
+        {
+            string queryParam = "Killswith";
+
+            //Arange
+            var bandServiceMock = new Mock<IBandService>();
+            var viewMock = new Mock<ISearchDisplayView>();
+            viewMock.Setup(x => x.Model).Returns(new SearchDisplayModel());
+
+            var eventArgs = new SearchDisplayEventArgs(queryParam);
+
+            //Act
+            var presenter = new SearchDisplayPresenter(viewMock.Object, bandServiceMock.Object);
+
+            // Assert
+            viewMock.Raise(v => v.OnSearchDisplayBands += null, eventArgs);
+
+            bandServiceMock.Verify(s => s.SearchBandsByBandName(It.IsAny<string>()), Times.Once());
+        }
+
+        [Test]
+        public void OnSearchDisplayBands_ShouldReturnBand()
+        {
+            string queryParam = "Killswith";
+            var bands = this.GetBands();
+            var expectedResult = bands.Where(b => b.BandName.Contains(queryParam));
+
+            //Arange
+            var bandServiceMock = new Mock<IBandService>();
+            var viewMock = new Mock<ISearchDisplayView>();
+            viewMock.Setup(x => x.Model).Returns(new SearchDisplayModel());
+
+            var eventArgs = new SearchDisplayEventArgs(queryParam);
+            bandServiceMock.Setup(s => s.SearchBandsByBandName(It.IsAny<string>())).Returns(bands.Where(b => b.BandName.Contains(queryParam)));
+
+            //Act
+            var presenter = new SearchDisplayPresenter(viewMock.Object, bandServiceMock.Object);
+
+            // Assert
+            viewMock.Raise(v => v.OnSearchDisplayBands += null, eventArgs);
+
+            CollectionAssert.AreEqual(expectedResult, presenter.View.Model.Bands);
+
+        }
+
+        public IEnumerable<Band> GetBands()
+        {
+            IList<Band> bands = new List<Band>()
+            {
+                new Band()
+                {
+                    Id = Guid.NewGuid(),
+                    BandName = "A day to remember",
+                    Country = new Country() { Id = Guid.NewGuid(), CountryName = "USA" },
+                    Genre = new Genre() { Id = Guid.NewGuid(), GenreName = "Metal" }
+                },
+                new Band()
+                {
+                    Id = Guid.NewGuid(),
+                    BandName = "Killswith Engage",
+                    Country = new Country() { Id = Guid.NewGuid(), CountryName = "USA" },
+                    Genre = new Genre() { Id = Guid.NewGuid(), GenreName = "Metal" }
+                },
+                new Band()
+                {
+                    Id = Guid.NewGuid(),
+                    BandName = "Aerosmith",
+                    Country = new Country() {Id = Guid.NewGuid(), CountryName = "USA" },
+                    Genre =new Genre() { Id = Guid.NewGuid(), GenreName = "Grindcore" }
+                },
+                new Band()
+                {
+                    Id = Guid.NewGuid(),
+                    BandName = "Behemoth",
+                    Country = new Country() { Id = Guid.NewGuid(), CountryName = "Finland" },
+                    Genre = new Genre() { Id = Guid.NewGuid(), GenreName = "Pop" }
+                }
+            };
+
+            return bands;
+        }
+    }
+}
